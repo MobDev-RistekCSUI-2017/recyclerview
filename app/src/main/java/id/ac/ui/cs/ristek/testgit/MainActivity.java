e81d98bfa4f54e8bca3d9400bfde011bae14526d
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2017-04-06 20:55:57 +0700

    update

--- app/src/main/java/id/ac/ui/cs/ristek/testgit/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/ristek/testgit/MainActivity.java
@@ -1,17 +1,30 @@
 package id.ac.ui.cs.ristek.testgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import id.ac.ui.cs.ristek.testgit.adapter.PixabayAdapter;
+import id.ac.ui.cs.ristek.testgit.model.Hit;
 import id.ac.ui.cs.ristek.testgit.model.Pixabay;
+import id.ac.ui.cs.ristek.testgit.model.ResponsePixabay;
+import id.ac.ui.cs.ristek.testgit.service.PixabayService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView RV  ;
+    public static final String BASE_URL = "https://pixabay.com/";
+
+    RecyclerView RV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +43,34 @@ public class MainActivity extends AppCompatActivity {
 
         RV.setLayoutManager(new LinearLayoutManager(this));
         RV.setAdapter(pixabayAdapter);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //https://pixabay.com/api/?key=3968517-94dbe52e08b9ec52249a64fdc&q=yellow+flowers&image_type=photo
+
+        PixabayService service = retrofit.create(PixabayService.class);
+        service.getImages("3968517-94dbe52e08b9ec52249a64fdc", "animal").enqueue(new Callback<ResponsePixabay>() {
+            @Override
+            public void onResponse(Call<ResponsePixabay> call, Response<ResponsePixabay> response) {
+                if (response.isSuccessful()) {
+                    List<Hit> hits = response.body().hits;
+                    for (int i = 0; i < hits.size(); i++) {
+                        Log.d("wahyu",hits.get(i).webformatURL);
+                        System.out.print(hits.get(i).webformatURL);
+                    }
+                    Toast.makeText(MainActivity.this, "berhasil coy", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponsePixabay> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
